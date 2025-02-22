c51b3f2722c4047afaefb4053d54e6fc0b8debc8
brunoeiterer <brunoeiterer@gmail.com>
2025-02-22 19:07:06 -0300

    Move add button to the gallery

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,12 +8,14 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.Storage.Pickers;
 using Microsoft.UI.Windowing;
+using System.Threading.Tasks;
 
 namespace PrintReady;
 
 public sealed partial class MainWindow : Window
 {
     private readonly ObservableCollection<Border> Images = [];
+    private int GallerySelectedIndex { get; set; } = -1;
 
     public MainWindow()
     {
@@ -22,7 +24,36 @@ public sealed partial class MainWindow : Window
         GalleryGrid.SelectionChanged += OnSelectionChange;
         GalleryGrid.ItemsSource = Images;
 
+        var AddImagesBorder = new Border()
+        {
+            Child = new FontIcon()
+            {
+                FontFamily = new FontFamily("Segoe Fluent Icons"),
+                Glyph = "\ue710",
+                FontSize = 64,
+                Width = 200,
+                Height = 200,
+            }
+        };
+
+        Images.Add(AddImagesBorder);
+
+        GalleryGrid.IsItemClickEnabled = true;
+        GalleryGrid.ItemClick += OnGalleryItemClicked;
+
         ((OverlappedPresenter)AppWindow.Presenter).Maximize();
+    }
+
+    private async void OnGalleryItemClicked(object sender, RoutedEventArgs e)
+    {
+        GalleryGrid.IsItemClickEnabled = false;
+
+        if(e is ItemClickEventArgs args && args.ClickedItem == Images[0])
+        {
+            await AddPictures();
+        }
+
+        GalleryGrid.IsItemClickEnabled = true;
     }
 
     private void LoadImages(IEnumerable<string> imagePaths)
@@ -50,15 +81,21 @@ public sealed partial class MainWindow : Window
 
     private void OnSelectionChange(object sender, SelectionChangedEventArgs e)
     {
-        var source = ((GalleryGrid.SelectedItem as Border)?.Child as Image)?.Source as BitmapImage;
-        Preview.ImagePath = source?.UriSource.LocalPath;
+        if(GalleryGrid.SelectedIndex == 0)
+        {
+            GalleryGrid.SelectedIndex = GallerySelectedIndex;
+        }
+        else
+        {
+            GallerySelectedIndex = GalleryGrid.SelectedIndex;
+
+            var source = ((GalleryGrid.SelectedItem as Border)?.Child as Image)?.Source as BitmapImage;
+            Preview.ImagePath = source?.UriSource.LocalPath;
+        }
     }
 
-    public async void OnAddPictures(object sender, RoutedEventArgs e)
+    public async Task AddPictures()
     {
-        var senderButton = (Button)sender;
-        senderButton.IsEnabled = false;
-
         var picker = new FileOpenPicker();
 
         var windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(this);
@@ -72,7 +109,5 @@ public sealed partial class MainWindow : Window
 
         var files = await picker.PickMultipleFilesAsync();
         LoadImages(files.Select(f => f.Path));
-
-        senderButton.IsEnabled = true;
     }
 }
